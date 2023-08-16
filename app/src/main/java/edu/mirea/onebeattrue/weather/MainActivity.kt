7225b902929e5d99c80f35a4ad1bf0705c4004e3
Vitaly <onebeattrue@gmail.com>
2023-08-17 02:18:57 +0300

    Реализация асинхронного программирования с возвращением в главный поток с помощью класса Handler

--- app/src/main/java/edu/mirea/onebeattrue/weather/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/weather/MainActivity.kt
@@ -2,10 +2,68 @@ package edu.mirea.onebeattrue.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.widget.Toast
+import androidx.core.view.isVisible
+import edu.mirea.onebeattrue.weather.databinding.ActivityMainBinding
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    private val handler = Handler()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.buttonLoad.setOnClickListener {
+            loadData()
+        }
+    }
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        loadCity { city: String ->
+            binding.tvLocation.text = city
+            loadTemperature(city) { temperature: Int ->
+                binding.tvTemperature.text = temperature.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadCity(callback: (String) -> Unit) {
+        thread {
+            Thread.sleep(5000)
+            handler.post {
+                callback.invoke("Moscow")
+            }
+        }
+    }
+
+    private fun loadTemperature(city: String, callback: (Int) -> Unit) {
+        thread {
+            handler.post {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+            Thread.sleep(5000)
+
+            handler.post(object : Runnable { // метод post принимает объект типа Runnable и вставляет его тело в основной поток
+                override fun run() {
+                    callback.invoke(17)
+                }
+            })
+        }
     }
 }
