c542e84cd9d0e58610b1d4262ccddf009f75458f
Vitaly <onebeattrue@gmail.com>
2023-08-18 16:50:17 +0300

    Разбор Coroutine под капотом: 1) фактически использование coroutine - программирование с callback'ами 2) suspend функции не должны блокировать поток 3) suspend функции в своей реализации используют state машину 4) suspend функции можно запустить только из другой suspend функции или coroutine'ы

--- app/src/main/java/edu/mirea/onebeattrue/weather/MainActivity.kt
+++ app/src/main/java/edu/mirea/onebeattrue/weather/MainActivity.kt
@@ -25,9 +25,10 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.buttonLoad.setOnClickListener {
-            lifecycleScope.launch {// теперь жизненный цикл запроса совпадает с жизненным циклом Activity
-                loadData()
-            }
+//            lifecycleScope.launch {// теперь жизненный цикл запроса совпадает с жизненным циклом Activity
+//                loadData()
+//            }
+            loadWithoutCoroutine()
         }
     }
 
@@ -42,6 +43,50 @@ class MainActivity : AppCompatActivity() {
         binding.buttonLoad.isEnabled = true
     }
 
+    private fun loadWithoutCoroutine(
+        step: Int = 0,
+        obj: Any? = null
+    ) { // примерно так выглядят coroutine под капотом
+        when (step) {
+            0 -> {
+                binding.progress.isVisible = true
+                binding.buttonLoad.isEnabled = false
+                loadCityWithoutCoroutine {
+                    loadWithoutCoroutine(1, it)
+                }
+            }
+
+            1 -> {
+                val city = obj as String
+                binding.tvLocation.text = city
+                loadTemperatureWithoutCoroutine(city) {
+                    loadWithoutCoroutine(2, it)
+                }
+            }
+
+            2 -> {
+                val temperature = obj as Int
+                binding.tvTemperature.text = temperature.toString()
+                binding.progress.isVisible = false
+                binding.buttonLoad.isEnabled = true
+            }
+        }
+    }
+
+    private fun loadCityWithoutCoroutine(callback: (String) -> Unit) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback("Moscow")
+        }, 5000)
+    }
+
+    private fun loadTemperatureWithoutCoroutine(city: String, callback: (Int) -> Unit) {
+        Toast.makeText(
+            this, getString(R.string.loading_temperature_toast, city), Toast.LENGTH_SHORT
+        ).show()
+        Handler(Looper.getMainLooper()).postDelayed({
+            callback(17)
+        }, 5000)
+    }
 
     private suspend fun loadCity(): String {
         delay(5000)
